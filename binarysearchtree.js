f5853e17b759a411a19c96c7b75acc514bce7d0f
javi <bzjvr@hotmail.com>
2023-11-02 20:53:35 -0700

    refactor: removed console.log testing at bottom to cleanup

--- binarysearchtree.js
+++ binarysearchtree.js
@@ -182,18 +182,3 @@ class BinaryTree {
     }
   }
 }
-
-
-let tree = new BinaryTree();
-tree.add(9);
-tree.add(4);
-tree.add(17);
-tree.add(3);
-tree.add(6);
-tree.add(22);
-tree.add(5);
-tree.add(7);
-tree.add(20);
-
-console.log(tree.findMinHeight());
-
